5e7cfa8aa74f33c6cffd7f207c390dd3cc5a8df4
CrazyBlond <blond007.valentina@gmail.com>
2022-10-01 00:33:47 +0300

    added popup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
 "use strict";
 
 $(function () {
+    setTimeout(showModal, 60000);
+    function showModal() {
+        const modalId = 'myMessageModal';
+        const newModal = $(`
+          <div class="modal fade" id="${modalId}" tabindex="-1">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="myModalLabel">Внимание! Картинка снизу!</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true"><i class="fas fa-times"></i></span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="image-wrapper">
+                        <img src=".//src/cat.svg" title="Картинка чего-то">
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Хорошо</button>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <div class="modal-backdrop fade show"></div>
+          `).appendTo('body');
+
+        const selector =  '#' + modalId;
+        $(selector).addClass('show');
+        $(selector).css('display', 'block');
+
+        $(selector + ' button[data-dismiss="modal"]').on("click", function (e) {
+            $(newModal).remove();
+        });
+    }
+
     $(".custom-select__title").text($(".custom-select__options").data("placeholder"));
 
     // Toggle menu
@@ -25,4 +61,6 @@ $(function () {
     $(".custom-range input[type=range]").on("input", function (e) {
         $(this).prevAll(".custom-label").children(".custom-label__range-value").text($(this).val() + " %");
     });
+
+
 });
